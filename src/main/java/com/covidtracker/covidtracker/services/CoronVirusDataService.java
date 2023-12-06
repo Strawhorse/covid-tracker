9513ccc39416cfc2c6c8a8d46ee3379138013e22
Sean B <strawhorse@gmail.com>
2023-12-06 21:25:44 +0000

    Building iterator (may not be used if can find package replacement) for COVID data from csv file

--- src/main/java/com/covidtracker/covidtracker/services/CoronVirusDataService.java
+++ src/main/java/com/covidtracker/covidtracker/services/CoronVirusDataService.java
@@ -1,7 +1,11 @@
 package com.covidtracker.covidtracker.services;
 
-import java.io.IOException;
-import java.net.URI;
+import org.apache.commons.csv.*;
+import org.apache.commons.csv.CSVRecord;
+
+
+import java.io.*;
+import java.net.*;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -15,9 +19,9 @@ public class CoronVirusDataService {
     //    Data just for testing purposes
     private List<LocationStats> allStats = new ArrayList<>();
 
-
 //    method to return all statistics
     public List<LocationStats> getAllStats() {
+//        method to return all the COVID stats
         return allStats;
     }
 
@@ -33,7 +37,24 @@ public class CoronVirusDataService {
 
 
         HttpResponse<String> httpResponse = (HttpResponse<String>) client.send(request, HttpResponse.BodyHandlers.ofString());
-        System.out.println(httpResponse);
+//        System.out.println(httpResponse);
+
+//        replace with a stringbuilder for the output
+        StringBuilder csvBodyReader = new StringBuilder(httpResponse.body());
+
+        CSVFormat records = CSVFormat.DEFAULT.withFirstRecordAsHeader();
+
+//        iterable loop to parse the csv data coming back in the CSV record
+        for(CSVRecord record: records){
+            LocationStats locationStat = new LocationStat();
+            locationStat.setState(record.get("Province/State"));
+            locationStat.setCountry(record.get("Country/Region"));
+            int latestCases = Integer.parseInt(record.get(record.size() - 1 ));
+            int previousDayCases = Integer.parseInt(record.get(record.size() - 2 ));
+            locationStat.setLatestTotalCases(latestCases);
+            locationStat.setDiffFromToday(latestCases - previousDayCases);
+            newStats.add(locationStat);
+        }
 
 
 
