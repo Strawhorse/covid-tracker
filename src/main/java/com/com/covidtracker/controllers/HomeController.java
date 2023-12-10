715af05f2a858f7220d3cf6cbd2d59543ecea0e9
Sean B <strawhorse@gmail.com>
2023-12-10 15:51:30 +0000

    Creating web html model

--- src/main/java/com/com/covidtracker/controllers/HomeController.java
+++ src/main/java/com/com/covidtracker/controllers/HomeController.java
@@ -1,29 +1,36 @@
 package com.com.covidtracker.controllers;
 
+import com.com.covidtracker.models.LocationStats;
+import com.com.covidtracker.services.CoronaVirusDataService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
+
+    @Autowired
+    CoronaVirusDataService coronVirusDataService;
+
     @GetMapping("/")
-    public String home() {
+    public String home(Model model){
+//        create a controller
+        List<LocationStats> allStats = coronVirusDataService.getAllStats();
+
+        int totalReportedCases = allStats.stream().mapToInt(stat -> stat.getLatestTotalCases()).sum();
+        int totalNewCases = allStats.stream().mapToInt(stat -> stat.getDiffFromPreviousDay()).sum();
+
+        model.addAttribute("LocationStats", allStats);
+        model.addAttribute("totalReportedCases", totalReportedCases);
+        model.addAttribute("totalNewCases", totalNewCases);
+
+//        create one html form using Thymeleaf to output all the data
+
         return "home";
     }
-//     creating a web app to control everything
-
-//    loading the service
-//    @Autowired
-//    CoronVirusDataService coronVirusDataService;
-//
-//    @GetMapping("/")
-//    public String home(Model model){
-////        create a controller
-//        List<LocationStats> allStats = coronVirusDataService.getAllStats();
-//
-////        create one html form using Thymeleaf to output all the data
-//
-//        return null;
-//    }
 
 
 }
